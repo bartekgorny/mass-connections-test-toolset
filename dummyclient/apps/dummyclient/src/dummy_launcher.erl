bab5c9d11bb8eca6c2034e45b48d33ee2cea4618
Ubuntu <ubuntu@ip-10-2-100-199.eu-central-1.compute.internal>
2020-05-28 11:13:13 +0000

    fixed conn counter in client, close socket

--- dummyclient/apps/dummyclient/src/dummy_launcher.erl
+++ dummyclient/apps/dummyclient/src/dummy_launcher.erl
@@ -18,32 +18,40 @@ prt(X) ->
 start() ->
     Opts = application:get_all_env(dummyclient),
     Max = proplists:get_value(max_conns, Opts),
+    ArriveInt = proplists:get_value(arrive_interval, Opts),
+    timer:send_interval(ArriveInt, new_connection),
 
-    start(1, Max, Opts),
+    start(1, Max, Opts, #{}),
     ok.
 
-start(Max, Max, _Opts) ->
+start(Max, Max, _Opts, _Errs) ->
     ok;
-start(Counter, Max, Opts) ->
-    Step = proplists:get_value(report_step, Opts),
-    case Counter rem Step of
-        0 ->
-            logger:notice("Conns: ~p", [Counter]),
-            report({running, Counter});
-        _ -> ok
-    end,
-    Me = self(),
-    ArriveInt = proplists:get_value(arrive_interval, Opts),
-    spawn_link(fun() -> start_client(Me, Opts) end),
+start(Counter, Max, Opts, Errs) ->
     receive
-        {over, Reason} ->
+        {failed, Reason} ->
+            logger:error("Failed because of: ~p", [Reason]),
+	    Errs1 = maps:put(Reason, maps:get(Reason, Errs, 0) + 1, Errs),
+            report({running, Counter, Errs}),
+	    start(Counter - 1, Max, Opts, Errs1),
+            ok;
+        {stopped, Reason} ->
             logger:error("Stopping because of: ~p", [Reason]),
-            report({over, Reason, Counter}),
-            ok
-        after ArriveInt ->
-            start(Counter + 1, Max, Opts)
+	    Errs1 = maps:put(Reason, maps:get(Reason, Errs, 0) + 1, Errs),
+            report({running, Counter, Errs}),
+	    start(Counter - 1, Max, Opts, Errs1),
+            ok;
+        new_connection ->
+            Step = proplists:get_value(report_step, Opts),
+            case Counter rem Step of
+                0 ->
+                    logger:notice("Conns: ~p", [Counter]),
+                    report({running, Counter, Errs});
+                _ -> ok
+            end,
+            Me = self(),
+            spawn_link(fun() -> start_client(Me, Opts) end),
+            start(Counter + 1, Max, Opts, Errs)
     end.
-
 
 start_client(Main, Opts) ->
     Host = proplists:get_value(host, Opts),
@@ -54,7 +62,7 @@ start_client(Main, Opts) ->
             loop(Main, Sock, MsgInt);
         {error, E} ->
             logger:error("Connection failed: ~p", [E]),
-            Main ! {over, {conn,  E }}
+            Main ! {failed, {conn,  E}}
     end.
 
 loop(Main, Sock, MsgInt) ->
@@ -65,11 +73,11 @@ loop(Main, Sock, MsgInt) ->
                     timer:sleep(MsgInt),
                     loop(Main, Sock, MsgInt);
                 {error, E} ->
-                    Main ! {over, {recv, E}},
+                    Main ! {stopped, {recv, E}},
                     logger:error("Recv failed: ~p", [E])
             end;
         {error, E} ->
-            Main ! {over, {send, E}},
+            Main ! {stopped, {send, E}},
             logger:error("Send failed: ~p", [E])
     end.
 
