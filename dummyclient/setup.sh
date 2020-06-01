089814546c714c12a63c5ad3d34de01e09600870
Ubuntu <ubuntu@ip-10-2-100-199.eu-central-1.compute.internal>
2020-06-01 12:39:10 +0000

    missing scripts

---
+++ dummyclient/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd _build/prod/rel/dummyclient/
+
+host=$(hostname -i)
+
+sed -i -e s/dummyclient$/dummyclient@$host/ releases/0.1.0/vm.args
+export HOME=/root
+ulimit -n 100000
+sysctl -w net.ipv4.ip_local_port_range="16000 61000"
+bin/dummyclient daemon &
